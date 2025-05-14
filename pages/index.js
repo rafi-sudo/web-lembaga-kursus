6dc9b2ec4d85a9cd2e319372c8eb25b3116347fa
Rafi Rasyadi <146502311+rafi-sudo@users.noreply.github.com>
2025-05-14 13:56:13 +0700

    create awal

---
+++ pages/index.js
@@ -0,0 +1,153 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <!-- frame: Board -->
+<div class="frame board-2e9483535c2f">
+  <!-- group: Group -->
+  <div class="group group-2ea0a56c380b">
+    <!-- path: Rectangle -->
+    <div class="shape path rectangle-2e953c3e3f48">
+      <svg width="1112" xmlns="http://www.w3.org/2000/svg" height="246" id="screenshot-64a63320-04cd-8057-8006-2e953c3e3f48" viewBox="454 -120 1112 246" style="-webkit-print-color-adjust::exact" xmlns:xlink="http://www.w3.org/1999/xlink" fill="none" version="1.1">
+        <g id="shape-64a63320-04cd-8057-8006-2e953c3e3f48">
+          <defs>
+            <filter id="filter-render-1503" x="-0.0263671875" y="-0.17088607594936708" width="1.064453125" height="1.4177215189873418" filterUnits="objectBoundingBox" color-interpolation-filters="sRGB">
+              <feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
+              <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"></feColorMatrix>
+              <feOffset dx="4" dy="4"></feOffset>
+              <feGaussianBlur stdDeviation="2"></feGaussianBlur>
+              <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"></feColorMatrix>
+              <feBlend mode="normal" in2="BackgroundImageFix" result="filter_64a63320-04cd-8057-8006-2e955760299e"></feBlend>
+              <feBlend mode="normal" in="SourceGraphic" in2="filter_64a63320-04cd-8057-8006-2e955760299e" result="shape"></feBlend>
+            </filter>
+            <filter id="filter-shadow-render-1503" x="-0.0263671875" y="-0.17088607594936708" width="1.064453125" height="1.4177215189873418" filterUnits="objectBoundingBox" color-interpolation-filters="sRGB">
+              <feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
+              <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"></feColorMatrix>
+              <feOffset dx="4" dy="4"></feOffset>
+              <feGaussianBlur stdDeviation="2"></feGaussianBlur>
+              <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"></feColorMatrix>
+              <feBlend mode="normal" in2="BackgroundImageFix" result="filter_64a63320-04cd-8057-8006-2e955760299e"></feBlend>
+              <feBlend mode="normal" in="SourceGraphic" in2="filter_64a63320-04cd-8057-8006-2e955760299e" result="shape"></feBlend>
+            </filter>
+            <linearGradient id="fill-color-gradient-render-1503-0" x1="0.5" y1="0" x2="0.5" y2="1" gradientTransform="">
+              <stop offset="0" stop-color="#d6d7e5" stop-opacity="1"></stop>
+              <stop offset="1" stop-color="#f4d406" stop-opacity="0.7055555555555556"></stop>
+            </linearGradient>
+            <pattern patternUnits="userSpaceOnUse" x="486" y="-88" width="1024" height="158" patternTransform="matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)" id="fill-0-render-1503">
+              <g>
+                <rect width="1024" height="158" style="fill:url(#fill-color-gradient-render-1503-0)"></rect>
+              </g>
+            </pattern>
+          </defs>
+          <g class="fills" id="fills-64a63320-04cd-8057-8006-2e953c3e3f48">
+            <path d="M486.000,-61.000L1510.000,-61.000L1510.000,70.000L486.000,70.000L486.000,-61.000ZM1391.000,24.000L1478.000,-88.000" filter="url(#filter-render-1503)" fill="url(#fill-0-render-1503)"></path>
+          </g>
+        </g>
+      </svg>
+    </div>
+    <!-- rect: logo_header -->
+    <div class="shape rect logoheade-2e961a0b02e3">
+    </div>
+    <!-- text: UPGRADE SKILL MU! -->
+    <div class="shape text upgrade-sk-2e966925680f">
+      <div class="text-node-html" id="html-text-node-64a63320-04cd-8057-8006-2e966925680f" data-x="636" data-y="-36">
+        <div class="root rich-text root-0" style="display:flex;white-space:break-spaces;align-items:flex-start" xmlns="http://www.w3.org/1999/xhtml">
+          <div class="paragraph-set root-0-paragraph-set-0">
+            <p class="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr"><span class="text-node root-0-paragraph-set-0-paragraph-0-text-0" style="color:rgba(17, 58, 85, 1);text-transform:none;line-break:auto;overflow-wrap:initial;white-space:break-spaces;font-size:50px;text-rendering:geometricPrecision;caret-color:rgba(17, 58, 85, 1);text-decoration:none;--font-id:gfont-texturina;--fills:[[&quot;^ &quot;,&quot;~:fill-color&quot;,&quot;#113a55&quot;,&quot;~:fill-opacity&quot;,1]];letter-spacing:0px;font-family:&quot;Texturina&quot;;font-style:normal;font-weight:400">UPGRADE SKILL MU!</span></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- text: KURSUS -->
+    <div class="shape text kursus-2e97a665626b">
+      <div class="text-node-html" id="html-text-node-64a63320-04cd-8057-8006-2e97a665626b" data-x="916" data-y="-1">
+        <div class="root rich-text root-0" style="display:flex;white-space:break-spaces;align-items:flex-start" xmlns="http://www.w3.org/1999/xhtml">
+          <div class="paragraph-set root-0-paragraph-set-0">
+            <p class="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr"><span class="text-node root-0-paragraph-set-0-paragraph-0-text-0" style="color:rgba(0, 0, 0, 1);text-transform:none;line-break:auto;overflow-wrap:initial;white-space:break-spaces;font-size:20px;text-rendering:geometricPrecision;caret-color:rgba(0, 0, 0, 1);text-decoration:none;--font-id:gfont-texturina;--fills:[[&quot;^ &quot;,&quot;~:fill-color&quot;,&quot;#000000&quot;,&quot;~:fill-opacity&quot;,1]];letter-spacing:0px;font-family:&quot;Texturina&quot;;font-style:normal;font-weight:400">KURSUS</span></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- text: PROGRAM -->
+    <div class="shape text program-2e97b86a2a1b">
+      <div class="text-node-html" id="html-text-node-64a63320-04cd-8057-8006-2e97b86a2a1b" data-x="1045" data-y="-1">
+        <div class="root rich-text root-0" style="display:flex;white-space:break-spaces;align-items:flex-start" xmlns="http://www.w3.org/1999/xhtml">
+          <div class="paragraph-set root-0-paragraph-set-0">
+            <p class="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr"><span class="text-node root-0-paragraph-set-0-paragraph-0-text-0" style="color:rgba(0, 0, 0, 1);text-transform:none;line-break:auto;overflow-wrap:initial;white-space:break-spaces;font-size:20px;text-rendering:geometricPrecision;caret-color:rgba(0, 0, 0, 1);text-decoration:none;--font-id:gfont-texturina;--fills:[[&quot;^ &quot;,&quot;~:fill-color&quot;,&quot;#000000&quot;,&quot;~:fill-opacity&quot;,1]];letter-spacing:0px;font-family:&quot;Texturina&quot;;font-style:normal;font-weight:400">PROGRAM</span></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- text: GALLERY ALUMNI -->
+    <div class="shape text gallery-al-2e97e323e748">
+      <div class="text-node-html" id="html-text-node-64a63320-04cd-8057-8006-2e97e323e748" data-x="1196" data-y="-1">
+        <div class="root rich-text root-0" style="display:flex;white-space:break-spaces;align-items:flex-start" xmlns="http://www.w3.org/1999/xhtml">
+          <div class="paragraph-set root-0-paragraph-set-0">
+            <p class="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr"><span class="text-node root-0-paragraph-set-0-paragraph-0-text-0" style="color:rgba(0, 0, 0, 1);text-transform:none;line-break:auto;overflow-wrap:initial;white-space:break-spaces;font-size:20px;text-rendering:geometricPrecision;caret-color:rgba(0, 0, 0, 1);text-decoration:none;--font-id:gfont-texturina;--fills:[[&quot;^ &quot;,&quot;~:fill-color&quot;,&quot;#000000&quot;,&quot;~:fill-opacity&quot;,1]];letter-spacing:0px;font-family:&quot;Texturina&quot;;font-style:normal;font-weight:400">GALLERY ALUMNI</span></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- text: TENTANG -->
+    <div class="shape text tentang-2e97fa0636a7">
+      <div class="text-node-html" id="html-text-node-64a63320-04cd-8057-8006-2e97fa0636a7" data-x="1343" data-y="-1">
+        <div class="root rich-text root-0" style="display:flex;white-space:break-spaces;align-items:flex-start" xmlns="http://www.w3.org/1999/xhtml">
+          <div class="paragraph-set root-0-paragraph-set-0">
+            <p class="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr"><span class="text-node root-0-paragraph-set-0-paragraph-0-text-0" style="color:rgba(0, 0, 0, 1);text-transform:none;line-break:auto;overflow-wrap:initial;white-space:break-spaces;font-size:20px;text-rendering:geometricPrecision;caret-color:rgba(0, 0, 0, 1);text-decoration:none;--font-id:gfont-texturina;--fills:[[&quot;^ &quot;,&quot;~:fill-color&quot;,&quot;#000000&quot;,&quot;~:fill-opacity&quot;,1]];letter-spacing:0px;font-family:&quot;Texturina&quot;;font-style:normal;font-weight:400">TENTANG</span></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- rect: Rectangle -->
+    <div class="shape rect rectangle-2e998dfbcf02">
+    </div>
+    <!-- rect: logo_bg -->
+    <div class="shape rect logobg-2e9a447f7e97">
+    </div>
+    <!-- rect: logo_footer -->
+    <div class="shape rect logofoote-2e9a9a8f1172">
+    </div>
+  </div>
+  <!-- group: Group -->
+  <div class="group group-2e9ce792ae94">
+    <!-- group: Group -->
+    <div class="group group-2e9b79da5c07">
+      <!-- frame: Board -->
+      <div class="frame board-2e9b71825a0a">
+        <!-- rect: Rectangle -->
+        <div class="shape rect rectangle-2e9b04ec6a3a">
+        </div>
+      </div>
+      <!-- text: IMG_MAPS -->
+      <div class="shape text imgmaps-2e9b1f47f027">
+        <div class="text-node-html" id="html-text-node-64a63320-04cd-8057-8006-2e9b1f47f027" data-x="1312" data-y="615">
+          <div class="root rich-text root-0" style="display:flex;white-space:break-spaces;align-items:flex-start" xmlns="http://www.w3.org/1999/xhtml">
+            <div class="paragraph-set root-0-paragraph-set-0">
+              <p class="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr"><span class="text-node root-0-paragraph-set-0-paragraph-0-text-0" style="color:rgba(0, 0, 0, 1);text-transform:none;line-break:auto;overflow-wrap:initial;white-space:break-spaces;font-size:20px;text-rendering:geometricPrecision;caret-color:rgba(0, 0, 0, 1);text-decoration:none;--font-id:gfont-texturina;--fills:[[&quot;^ &quot;,&quot;~:fill-color&quot;,&quot;#000000&quot;,&quot;~:fill-opacity&quot;,1]];letter-spacing:0px;font-family:&quot;Texturina&quot;;font-style:normal;font-weight:400">IMG_MAPS</span></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- text: Office Multi Skill
+Jl. Lorem Ipsum, Dolor, Sit Amet
+E-Mail : loremipsum@gmail.com
+Telp/WhatsApp : 081 xx xx x -->
+    <div class="shape text office-mul-2e9bad460fe2">
+      <div class="text-node-html" id="html-text-node-64a63320-04cd-8057-8006-2e9bad460fe2" data-x="636" data-y="587">
+        <div class="root rich-text root-0" style="display:flex;white-space:break-spaces;align-items:flex-start" xmlns="http://www.w3.org/1999/xhtml">
+          <div class="paragraph-set root-0-paragraph-set-0">
+            <p class="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr"><span class="text-node root-0-paragraph-set-0-paragraph-0-text-0" style="color:rgba(255, 255, 255, 1);text-transform:none;line-break:auto;overflow-wrap:initial;white-space:break-spaces;font-size:20px;text-rendering:geometricPrecision;caret-color:rgba(255, 255, 255, 1);text-decoration:none;--font-id:gfont-texturina;--fills:[[&quot;^ &quot;,&quot;~:fill-color&quot;,&quot;#ffffff&quot;,&quot;~:fill-opacity&quot;,1]];letter-spacing:0px;font-family:&quot;Texturina&quot;;font-style:normal;font-weight:400">Office Multi Skill</span></p>
+            <p class="paragraph root-0-paragraph-set-0-paragraph-1" dir="ltr"><span class="text-node root-0-paragraph-set-0-paragraph-1-text-0" style="color:rgba(255, 255, 255, 1);text-transform:none;line-break:auto;overflow-wrap:initial;white-space:break-spaces;font-size:20px;text-rendering:geometricPrecision;caret-color:rgba(255, 255, 255, 1);text-decoration:none;--font-id:gfont-texturina;--fills:[[&quot;^ &quot;,&quot;~:fill-color&quot;,&quot;#ffffff&quot;,&quot;~:fill-opacity&quot;,1]];letter-spacing:0px;font-family:&quot;Texturina&quot;;font-style:normal;font-weight:400">Jl. Lorem Ipsum, Dolor, Sit Amet</span></p>
+            <p class="paragraph root-0-paragraph-set-0-paragraph-2" dir="ltr"><span class="text-node root-0-paragraph-set-0-paragraph-2-text-0" style="color:rgba(255, 255, 255, 1);text-transform:none;line-break:auto;overflow-wrap:initial;white-space:break-spaces;font-size:20px;text-rendering:geometricPrecision;caret-color:rgba(255, 255, 255, 1);text-decoration:none;--font-id:gfont-texturina;--fills:[[&quot;^ &quot;,&quot;~:fill-color&quot;,&quot;#ffffff&quot;,&quot;~:fill-opacity&quot;,1]];letter-spacing:0px;font-family:&quot;Texturina&quot;;font-style:normal;font-weight:400">E-Mail : loremipsum@gmail.com</span></p>
+            <p class="paragraph root-0-paragraph-set-0-paragraph-3" dir="ltr"><span class="text-node root-0-paragraph-set-0-paragraph-3-text-0" style="color:rgba(255, 255, 255, 1);text-transform:none;line-break:auto;overflow-wrap:initial;white-space:break-spaces;font-size:20px;text-rendering:geometricPrecision;caret-color:rgba(255, 255, 255, 1);text-decoration:none;--font-id:gfont-texturina;--fills:[[&quot;^ &quot;,&quot;~:fill-color&quot;,&quot;#ffffff&quot;,&quot;~:fill-opacity&quot;,1]];letter-spacing:0px;font-family:&quot;Texturina&quot;;font-style:normal;font-weight:400">Telp/WhatsApp : 081 xx xx x</span></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+  );
+}
+
+export default Home;
